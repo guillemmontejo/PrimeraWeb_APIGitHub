654901043fce9751e54c13466ea4f824204ae422
Guille Montejo <guillemmontejo@gmail.com>
2020-10-14 14:00:21 +0200

    14/10/2020 14:00

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 var obj;
 $(document).ready(function(){
   $(".submit").click(function(){
-    $("#table").empty();
-    $.get( "https://api.github.com/users/jlopezr/repos", function( data ) {
-      alert(data);
-    });
-    /*$.getJSON("https://api.github.com/users/" + $("#uss").val() + "/repos","", function Recibido(json){
+    $.getJSON("https://api.github.com/users/" + $("#uss").val() + "/repos","", function Recibido(json){
       obj = json;
       var len = obj.length;
       var i;
-      $("#table").append("<tr><th>Repo Name</th><th>Description</th><th>Followers</th></tr>");
+      //$("#table").append("<tr><th>Repo Name</th><th>Description</th><th>Followers</th></tr>");
       for (i = 0; i < len; i++){
         $("#table").append((" <tr><td>" + obj[i].name + "</td><td>" + obj[i].description + "</td><td>" + obj[i].watchers + "</td></tr>"));
       }
-    );
-    */
+    });
     $("#table").show();
   });
 });
