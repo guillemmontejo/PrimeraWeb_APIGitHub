80d95498847a9f2740df28af3d716fc418b35a6c
Guille Montejo <guillemmontejo@gmail.com>
2020-10-17 16:38:58 +0200

    Last changes. It works correctly.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
       obj = json;
       var len = obj.length;
       var i;
-      //$("#table").append("<tr><th>Repo Name</th><th>Description</th><th>Followers</th></tr>");
+      $("#table td").remove(); 
       for (i = 0; i < len; i++){
         $("#table").append((" <tr><td>" + obj[i].name + "</td><td>" + obj[i].description + "</td><td>" + obj[i].watchers + "</td></tr>"));
       }
